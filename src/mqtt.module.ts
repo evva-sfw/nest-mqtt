ab0be307173c75b108167d9a34ad305a0c8ee851
Dieter Wimberger <dieter@wimpi.net>
2024-12-09 16:10:51 +0100

    refactor: added subscription handling through annotations; added first e2e test against local mqtt broker

--- src/mqtt.module.ts
+++ src/mqtt.module.ts
@@ -4,10 +4,12 @@ import {
 } from '@nestjs/common';
 import { MqttService } from './mqtt.service';
 import { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } from './mqtt.module-definition';
+import { DiscoveryModule, DiscoveryService } from '@nestjs/core';
 
 @Global()
 @Module({
   providers: [MqttService],
+  imports: [DiscoveryModule],
   exports: [MqttService, MODULE_OPTIONS_TOKEN],
 })
 export class MqttModule extends ConfigurableModuleClass {
