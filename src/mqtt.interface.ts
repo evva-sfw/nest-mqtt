bd60f7c1fe328644a15949518c0eb77c957bb26d
Dieter Wimberger <dieter@wimpi.net>
2025-03-18 19:22:44 +0100

    feat: allows variables in topics in subscription decorators
    Add capability to add variables to topics in subscription decorators e.g. test/{variable}/blubb

--- src/mqtt.interface.ts
+++ src/mqtt.interface.ts
@@ -2,52 +2,59 @@ import { IClientOptions, MqttProtocol, Packet } from 'mqtt/*';
 
 export interface MqttModuleOptions extends IClientOptions {
 
-    /**
-     * MQTT Broker host
-     */
-    host: string;
-    /**
-     * MQTT Broker port
-     */
-    port: number;
-    /**
-     * MQTT Broker protocol
-     */
-    protocol: MqttProtocol;
-    /**
-     * MQTT username
-     */
-    username: string;
-    /**
-     * Mqtt password
-     */
-    password?: string;
+  /**
+   * MQTT Broker host
+   */
+  host: string;
+  /**
+   * MQTT Broker port
+   */
+  port: number;
+  /**
+   * MQTT Broker protocol
+   */
+  protocol: MqttProtocol;
+  /**
+   * MQTT username
+   */
+  username: string;
+  /**
+   * Mqtt password
+   */
+  password?: string;
 
-    /**
-     * Mqtt password provider; may be used to obtain a password.
-     * Will be called upon reconnect.
-     * @returns
-     */
-    passwordProvider?: () => Promise<string>;
+  /**
+   * Mqtt password provider; may be used to obtain a password.
+   * Will be called upon reconnect.
+   * @returns
+   */
+  passwordProvider?: () => Promise<string>;
 
-    /**
-     * Global queue subscribe.
-     * All topic will be prepend '$queue/' prefix automatically.
-     * More information is here:
-     * https://docs.emqx.io/broker/latest/cn/advanced/shared-subscriptions.html
-     */
-    queue?: boolean;
+  /**
+   * Mqtt topic resolver; may be used to resolve a topic variable
+   * at runtime. Will be called on explore/processing decorators.
+   * @returns the value as {string}.
+   */
+  topicResolver?: (varname: string) => string;
 
-    /**
-     * Global shared subscribe.
-     * All topic will be prepend '$share/group/' prefix automatically.
-     * More information is here:
-     * https://docs.emqx.io/broker/latest/cn/advanced/shared-subscriptions.html
-     */
-    share?: string;
+  /**
+   * Global queue subscribe.
+   * All topic will be prepend '$queue/' prefix automatically.
+   * More information is here:
+   * https://docs.emqx.io/broker/latest/cn/advanced/shared-subscriptions.html
+   */
+  queue?: boolean;
 
-    topic?: string;
-    beforeHandle?: (topic: string, payload: Buffer, packet: Packet) => any;
+  /**
+   * Global shared subscribe.
+   * All topic will be prepend '$share/group/' prefix automatically.
+   * More information is here:
+   * https://docs.emqx.io/broker/latest/cn/advanced/shared-subscriptions.html
+   */
+  share?: string;
+
+  topic?: string;
+  beforeHandle?: (topic: string, payload: Buffer, packet: Packet) => any;
 }
 
 export type MqttMessageTransformer = (payload: Buffer) => any;
